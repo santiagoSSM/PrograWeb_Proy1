85a50fcf824ec06c871e224a37700c96746a91a0
Adrian Leandro <>
2018-03-21 10:07:11 -0600

    Ayuda
    Creacion de ayuda

---
+++ ayuda.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="styles/normalize.css" type="text/css" media="all" />
+		<link rel="stylesheet" href="styles/style.css" type="text/css" media="all" />
+		<title>Ayuda</title>
+		
+	</head>
+	<body>
+		<main >
+			
+				<table>
+				    <tr>
+				        <td><h1>Ayuda</h1></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td><h2>Ingreso al sistema</h2></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td><p>Para poder ingresar al sistema deberá crear un usuario haciendo click en el enlace 'Crear cuenta' para posteriormente ingresar 
+								con su usuario y contraseña al administrador de archivos de Excel.</p>
+						    <p>El formulario de registro uicamente le pedira ingresar un usuario y una contraseña.</p>
+						</td>
+						<td><img src="images/ayuda/ingreso.JPG"></td>
+					</tr>
+					
+				</table>
+				
+				<table>
+					<tr>
+						<td><h2>Uso del administrador de archivos</h2></td>
+					</tr>
+					<tr>
+						<td>
+							<p><img src="images/ayuda/administrador.JPG" width="100%"></p>
+							<p>Habiendo ingresado su usuario correctamente, se le mostrara la seccion del administrador de archivos tal y como se ilustra en la imagen anterior.</p>
+						</td>
+					</tr>
+				</table>
+				<table>
+					<tr>
+						<td>
+							<p>Hacienda click en el boton 'Nuevo', podra subir un archivo a su cuenta.</p>
+						    <p>Se le mostraran varios campos que debe llenar para poder subir su archivo de Excel. Si intenta subir un archivo distinto a Excel, el sistema le indicara que el archvio no es compatible</p>
+						</td>
+						<td><img src="images/ayuda/nuevo.JPG" width="300px"></td>
+					</tr>
+				</table>
+			
+			
+			
+		</main>
+	</body>
+
+</html>
